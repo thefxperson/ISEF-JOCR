48dab3e450e0196e61445c93c25b9d5e58318ce6
thefxperson <thefxperson@gmail.com>
2017-11-11 21:11:09 -0800

    started maximum likelihood estimation

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
-import tensorflow as tf
+import idx2numpy as idx
+import numpy as np
+import numpy.ma as ma
+import matplotlib.pyplot as plt
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+labels = idx.convert_from_file("MNIST_data/train-labels.idx1-ubyte")
+images = idx.convert_from_file("MNIST_data/train-images.idx3-ubyte")
 
-sess = tf.InteractiveSession()
+#images [images number][x][y]
+#labels [label number]
 
-x= tf.placeholder(tf.float32, shape=[None, 784])	#input
-y_ = tf.placeholder(tf.float32, shape=[None, 10])	#output
+def norm_dist(x,std,mu):
+	expo = (-1*np.power((x-mu),2))/(2*np.power(std,2))
+	coef = 1/(np.sqrt(2*np.pi)*std)
+	return coef*np.exp(expo)
 
-W = tf.Variable(tf.zeros([784, 10]))	#weights
-b = tf.Variable(tf.zeros([10]))			#biases
+def clear_foo(foo):
+	for i in range(0,10):
+		for j in range(0,100):
+			foo[i][j] = np.nan
 
-sess.run(tf.global_variables_initializer())	#inits all vars
+	return foo
 
-y = tf.matmul(x,W) + b
+def writeVals(std,mu):
+	np.save("MNIST_data/std.npy",std)
+	np.save("MNIST_data/mu.npy",mu)
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+dataset = np.array([1,2,3,4,5])
 
-#train
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+std_data = np.empty([10,28,28])#[catagory][x][y]
+mu_data = np.empty([10,28,28])
+foo = np.empty([10,100])
 
-for _ in range(1000):
-	batch = mnist.train.next_batch(100)
-	train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+foo = clear_foo(foo)
 
-#get accuracy
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+for i in range(0, images.shape[1]):
+	for j in range(0, images.shape[2]):
+		for k in range(0, 100):
+			 foo[labels[k]][k] = images[k][i][j]
+
+		#mx = ma.masked_values(foo, np.nan)	#mx is an array without -inf(numbers that weren't filled in)
+		mx = ma.masked_invalid(foo)
+
+		for l in range(0, 10):	#allow calculation for each number
+			mu_data[l][i][j] = np.mean(mx[l])	#mean of each pixel
+			std_data[l][i][j] = np.std(mx[l])	#std of each pixel
+
+		foo = clear_foo(foo)
+
+writeVals(std_data,mu_data)
+
+#show the image (first arg)
+plt.imshow(std_data[9],cmap=plt.cm.gray_r,interpolation="nearest")
+plt.show()
