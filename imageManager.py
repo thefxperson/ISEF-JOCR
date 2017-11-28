5646af90b1e97ad443f79ba231cf7e36a884301c
thefxperson <thefxperson@gmail.com>
2017-11-27 19:51:45 -0800

    finished alphahot encoder

--- imageManager.py
+++ imageManager.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
 import random
 from datetime import datetime
 
 class imageManager():
 	def __init__(self):
-		random.seed(datetime.now())
+		random.seed(datetime.now())						#seed based on current time... ensures that each episode is random
 
-	def importImgs(self,classID,imgSize=105):
+	def importImgs(self,classID,imgSize=105):			#import 
 		#determine if testing image or training image
 		if classID <= 964:
 			imgPath = "images_background"
@@ -51,36 +50,49 @@ class imageManager():
 			
 
 	def getEpisode(self, numClasses, imgSize=105, train=True):
+		random.seed(datetime.now())				#seed based on current time... ensures that each episode is random
 		#prep for test or train episode
-		if train:
+		if train:				#training class range...see Omniglot_data/README.md
 			lower = 0
 			upper = 964
-		else:
+		else:					#testing class range
 			lower = 965
 			upper = 1623
 
 		classList = random.sample(range(lower,upper+1),numClasses)	#generate random classes totalling to numClasses
 
 		#arrays of images and labels for the episode
-		episodeImgs = np.zeros((10*numClasses,imgSize,imgSize))
-		episodeLabels = np.zeros(10*numClasses)
+		episodeImgs = np.zeros((10*numClasses,imgSize,imgSize))		#array of zeros to hold images
+		episodeLabels = np.empty(10*numClasses,dtype=object)		#empty array to hold labels
+		alpha = self.alphaHot(numClasses)							#generate the random alphahot labels to use for this ep
+
+		chooseClass = [random.randint(0,numClasses-1) for i in range(10*numClasses)]	#choose classes randomly
+		print(chooseClass)
 
 		for i in range(10*numClasses):
-			chooseClass = random.randint(0,numClasses-1)	#choose which class, returns var from 0-numClasses
-			episodeImgs[i] = self.importImgs(classList[chooseClass])	#gets a random image from chosen class, using classDict
-			'''episodeLabels[i] = alphaHot(chooseClass)	'''#encodes the label as alphaHot type
-			episodeLabels[i] = classList[chooseClass]
+			episodeImgs[i] = self.importImgs(classList[chooseClass[i]])	#gets a random image from chosen class
+			episodeLabels[i] = alpha[chooseClass]				#encodes label with alpha hot
 
-		return episodeImgs, episodeLabels				#returns images and labelss
+		#return episodeImgs, episodeLabels				#returns images and labelss
 
+	#generate unique labels for given number of classes in alphahot encoding
+	def alphaHot(self, numClasses):
+		order = random.sample(range(15625),numClasses)	#chooses unique random labels of the number of classes		15625 is the largest value possible with 6 letters
+		hot = []
+		for i in range(numClasses):
+			baseFive = self.changeBase(order[i])		#changeBase returns an array, thus is called 1x per loop to get new array
+			foo = ""
+			for j in range(6):
+				foo += chr(97+baseFive[j])					#change the base from 10 to 5 so it can be encoded using letters a->e
+			hot.append(foo)
+		return hot
 
-	#need x unique labels, where each label has 5 unqiue vals
-	def alphaHot(self, classList):	#encodes label with alphahot
-		order = random.sample(range(0,5),5)
-		hot = ""
-		for j in range(numClasses):				#convert to string (97 is lowercase a)
-			hot += chr(97+order[j])
-		print(hot)
+	def changeBase(self, number):	#changes a number in base 10 to a number in base 5
+		remainder = []
+		for i in range(6):
+			remainder.append(number % 5)	#mod func returns remainder
+			number = int(number / 5)
+		return remainder[::-1]			#reverse nums
 
-imgM = imageManager()
-#imgs, labels = imgM.getEpisode(5)
+foo = imageManager()
+foo.getEpisode(2)
