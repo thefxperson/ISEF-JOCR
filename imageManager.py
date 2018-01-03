45c8c1ca58a3a3c766ef9ecb3e92954279e02e8f
thefxperson <thefxperson@gmail.com>
2018-01-02 22:32:41 -0800

    debugged to get MANN working

--- imageManager.py
+++ imageManager.py
@@ -63,19 +63,18 @@ class imageManager():
 
 		#arrays of images and labels for the episode
 		episodeImgs = np.zeros((10*numClasses,20,20))		#array of zeros to hold images
-		episodeLabels = np.empty(10*numClasses,dtype=object)		#empty array to hold labels
-		alpha = self.alphaHot(numClasses)							#generate the random alphahot labels to use for this ep
+		#episodeLabels = np.empty(10*numClasses,dtype=object)		#empty array to hold labels
+		#alpha = self.alphaHot(numClasses)							#generate the random alphahot labels to use for this ep
 		rotation = [random.randint(0,3) for i in range(numClasses)]	#rotates each class randomly by 90deg.
-		imgNums = random.sample(range(20),10)						#generates 10 unique numbers for the image so the same image isn't used twice
+		imgNums = [random.randint(1,20) for i in range(10*numClasses)]#temprandom.sample(range(20),10)						#generates 10 unique numbers for the image so the same image isn't used twice
 
-		chooseClass = [random.randint(0,numClasses-1) for i in range(10*numClasses)]	#choose classes randomly
-
+		chooseClass = [random.randint(1,numClasses-1) for i in range(10*numClasses)]	#choose classes randomly
 		for i in range(10*numClasses):
 			episodeImgs[i] = self.adjustImg(sp.interpolation.rotate(self.importImgs(classList[chooseClass[i]],imgNums[i]), rotation[chooseClass[i]]*90))		#get random image from chosen class, rotates that by 0, 90, 180, or 270 deg. 
 																																					#Then randomly shifts and rotates by +-10px/+-10deg, and downscale to 20x20
-			episodeLabels[i] = alpha[chooseClass[i]]				#encodes label with alpha hot
+			#episodeLabels[i] = alpha[chooseClass[i]]				#encodes label with alpha hot
 
-		return episodeImgs, episodeLabels				#returns images and labelss
+		return episodeImgs#, episodeLabels				#returns images and labelss
 
 	#generate unique labels for given number of classes in alphahot encoding
 	def alphaHot(self, numClasses):
@@ -101,32 +100,28 @@ class imageManager():
 		image = sp.interpolation.shift(image, random.randint(-10,10))		#shift image between -10 and 10 pixels
 		return spmc.imresize(image, (20,20))
 
-def getLoss(prediction, labels):            #predictions[30], labels[6]
-    #convert from alpha to one hot
-    one = [[None]*5 for _ in range(6)] 
-    labels = list(labels[0])
-    print(len(labels))
-    for i in range(len(labels)):         #converts to labels[6][5]
-        if labels[i] == "a":
-            one[i] = [1,0,0,0,0]
-        elif labels[i] == "b":
-            one[i] = [0,1,0,0,0]
-        elif labels[i] == "c":
-            one[i] = [0,0,1,0,0]
-        elif labels[i] == "d":
-            one[i] = [0,0,0,1,0]
-        elif labels[i] == "e":
-            one[i] = [0,0,0,0,1]
-    one = np.reshape(one, 30)
-    #get loss
-    diff = one - prediction
-    diff = np.absolute(diff)
-    print(diff)
-    loss = -1*np.log10(diff)        #log0 = inf
-    print(loss)
-    loss[np.isinf(loss)] = 0        #replace inf with 0
-    return np.sum(loss)
-
-a = np.array([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0])
-b = ["abaaaa"]
-print(getLoss(a,b))
+	def getLoss(prediction, labels):            #predictions[30], labels[6]
+		#convert from alpha to one hot
+		one = [[None]*5 for _ in range(6)] 
+		labels = list(labels[0])
+		print(len(labels))
+		for i in range(len(labels)):         #converts to labels[6][5]
+			if labels[i] == "a":
+				one[i] = [1,0,0,0,0]
+			elif labels[i] == "b":
+				one[i] = [0,1,0,0,0]
+			elif labels[i] == "c":
+				one[i] = [0,0,1,0,0]
+			elif labels[i] == "d":
+				one[i] = [0,0,0,1,0]
+			elif labels[i] == "e":
+				one[i] = [0,0,0,0,1]
+		one = np.reshape(one, 30)
+		#get loss
+		diff = one - prediction
+		diff = np.absolute(diff)
+		print(diff)
+		loss = -1*np.log10(diff)        #log0 = inf
+		print(loss)
+		loss[np.isinf(loss)] = 0        #replace inf with 0
+		return np.sum(loss)
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def cosine_similarity(x, y, eps=1e-6):
 	return z
 
 def shared_float32(x, name=""):
-	return tf.Variable(tf.cast(np.asarray(x, dttype=np.float32), tf.float32), name=name)
+	return tf.Variable(tf.cast(np.asarray(x, dtype=np.float32), tf.float32), name=name)
 
 def update_tensor(V, dim2, val):
 	val = tf.cast(val, V.dtype)
@@ -26,7 +26,7 @@ def update_tensor(V, dim2, val):
 		inb = lambda : tf.slice(tf.concat([v[:d2_int], chg, v[d2_int + 1:]], axis=0), tf.constant(0, shape=[len(v.get_shape().as_list())]), v.get_shape().as_list())
 		return tf.cond(tf.less(d2_int + 1, v.get_shape().as_list()[0]), inb, oob)
 
-	Z = tf.scan(body, elems=(V, dim2, val), initalizer=tf.constant(1, shape=V.get_shape().as_list()[1:], dtype=tf.float32), name="Scan_Update")
+	Z = tf.scan(body, elems=(V, dim2, val), initializer=tf.constant(1, shape=V.get_shape().as_list()[1:], dtype=tf.float32), name="Scan_Update")
 	return Z
 
 def shared_glorot_uniform(shape, dtype=tf.float32, name='', n=None):
@@ -53,35 +53,35 @@ def weight_and_bias_init(shape, dtype=tf.float32, name='', n=None):
 	return (shared_glorot_uniform(shape, dtype=dtype, name='W_' + name, n=n), shared_zeros((shape[1],), dtype=dtype, name='b_' + name, n=n))
 
 def accuracy_instance(predictions, targets, n=[1, 2, 3, 4, 5, 10], nb_classes=5, nb_samples_per_class=10, batch_size=1):
-    targets = tf.cast(targets, predictions.dtype)
+	targets = tf.cast(targets, predictions.dtype)
 
-    accuracy = tf.constant(value=0, shape=(batch_size, nb_samples_per_class), dtype=tf.float32)
-    indices = tf.constant(value=0, shape=(batch_size, nb_classes+1), dtype=tf.float32)
+	accuracy = tf.constant(value=0, shape=(batch_size, nb_samples_per_class), dtype=tf.float32)
+	indices = tf.constant(value=0, shape=(batch_size, nb_classes+1), dtype=tf.float32)
 
-    def step_(step_arg1, step_arg2):
-        accuracy, indices = step_arg1
-        p, t = step_arg2
+	def step_(step_arg1, step_arg2):
+		accuracy, indices = step_arg1
+		p, t = step_arg2
 
-        p = tf.cast(p, tf.int32)
-        t = tf.cast(t, tf.int32)
-        ##Accuracy Update
-        batch_range = tf.cast(tf.range(0, batch_size), dtype=tf.int32)
-        gather = tf.cast(tf.gather_nd(indices,tf.stack([tf.range(0,p.get_shape().as_list()[0]), t], axis=1)), tf.int32)
-        index = tf.cast(tf.stack([batch_range, gather], axis=1), dtype=tf.int64)
-        val = tf.cast(tf.equal(p, t), tf.float32)
-        delta = tf.SparseTensor(indices=index, values=val, dense_shape=tf.cast(accuracy.get_shape().as_list(), tf.int64))
-        accuracy = accuracy + tf.sparse_tensor_to_dense(delta)
-        ##Index Update
-        index = tf.cast(tf.stack([batch_range, t], axis=1), dtype=tf.int64)
-        val = tf.constant(1.0, shape=[batch_size])
-        delta = tf.SparseTensor(indices=index, values=val, dense_shape=tf.cast(indices.get_shape().as_list(), dtype=tf.int64))
-        indices = indices + tf.sparse_tensor_to_dense(delta)
-        return [accuracy, indices]
+		p = tf.cast(p, tf.int32)
+		t = tf.cast(t, tf.int32)
+		##Accuracy Update
+		batch_range = tf.cast(tf.range(0, batch_size), dtype=tf.int32)
+		gather = tf.cast(tf.gather_nd(indices,tf.stack([tf.range(0,p.get_shape().as_list()[0]), t], axis=1)), tf.int32)
+		index = tf.cast(tf.stack([batch_range, gather], axis=1), dtype=tf.int64)
+		val = tf.cast(tf.equal(p, t), tf.float32)
+		delta = tf.SparseTensor(indices=index, values=val, dense_shape=tf.cast(accuracy.get_shape().as_list(), tf.int64))
+		accuracy = accuracy + tf.sparse_tensor_to_dense(delta)
+		##Index Update
+		index = tf.cast(tf.stack([batch_range, t], axis=1), dtype=tf.int64)
+		val = tf.constant(1.0, shape=[batch_size])
+		delta = tf.SparseTensor(indices=index, values=val, dense_shape=tf.cast(indices.get_shape().as_list(), dtype=tf.int64))
+		indices = indices + tf.sparse_tensor_to_dense(delta)
+		return [accuracy, indices]
 
-    accuracy, indices = tf.scan(step_, elems=(tf.transpose(predictions, perm=[1, 0]), tf.transpose(targets, perm=[1, 0])),initializer=[accuracy, indices], name="Scan_Metric_Last")
+	accuracy, indices = tf.scan(step_, elems=(tf.transpose(predictions, perm=[1, 0]), tf.transpose(targets, perm=[1, 0])),initializer=[accuracy, indices], name="Scan_Metric_Last")
 
-    accuracy = accuracy[-1]
+	accuracy = accuracy[-1]
 
-    accuracy = tf.reduce_mean(accuracy / nb_classes , axis=0)
+	accuracy = tf.reduce_mean(accuracy / nb_classes , axis=0)
 
-return accuracy
+	return accuracy
